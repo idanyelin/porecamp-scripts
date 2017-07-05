b28b5089e6cc861cfad06a51f92926d3a4f5dc7f
mjpdejong <mail@mjpdejong.com>
2017-07-05 15:25:19 +0000

    2nd commit

--- 4_alignment/bwa.sh
+++ 4_alignment/bwa.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
+#input file
 input="/home/group8/1_trimming/output/barcode06/bundled.fastq"
 
+#indexing the reference file
 samtools index hepc_ref.fasta
 
+#select 1k reads and output in a new fastq
 cat $input | head -n 4000 > reads.fastq
 
+#alginment
 bwa mem -t 20 -x ont2d hepc_ref.fasta reads.fastq > hepc.sam
 
+#convert sam to bam
 samtools view -bS hepc.sam > hepc.bam
 
+#sort bamfile
 samtools sort hepc.bam > hepc.sorted.bam
 
+#index bamfile
 samtools index hepc.sorted.bam
